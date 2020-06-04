8650763c8a161e2df9589b76eaf31d574d07f93b
devuri <devuriko@gmail.com>
2020-06-04 14:05:27 -0500

    resolving conflicts between hotfix and master

--- footer.php
+++ footer.php
@@ -9,7 +9,9 @@
  * @package brisko
  */
 
-?><footer id="colophon" class="site-footer white-bg <?php brisko_secondary_font(); ?>">
+?>
+<?php brisko_before_footer(); ?>
+<footer id="colophon" class="site-footer white-bg <?php brisko_secondary_font(); ?>">
 		<div align="center" class="site-info container">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'brisko' ) ); ?>">
 			 <?php printf( esc_html__( 'Powered by %s', 'brisko' ), 'WordPress' );?>
