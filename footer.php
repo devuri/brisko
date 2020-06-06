5b09faae3a6fbbafac375dee956e3701c9ec06b2
devuri <devuriko@gmail.com>
2020-06-06 13:35:54 -0500

    brisko_footer() and brisko_after_footer() actions

--- footer.php
+++ footer.php
@@ -8,10 +8,10 @@
  *
  * @package brisko
  */
-
 ?>
 <?php brisko_before_footer(); ?>
 <footer id="colophon" class="site-footer bg-white secondary-font">
+	<?php brisko_footer(); ?>
 		<div align="center" class="site-info container">
 				<?php
 				printf( esc_html__( 'Copyright &copy; %1$s %2$s. Theme %3$s by %4$s.', 'brisko' ), date_i18n( __( 'Y' , 'brisko' ) ), get_bloginfo( 'name' ), 'Brisko', '<a href="http://themeiko.com">Themeiko</a>' );
@@ -20,5 +20,6 @@
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 <?php wp_footer(); ?>
+<?php brisko_after_footer(); ?>
 </body>
 </html>
