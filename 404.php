8fbd36db7e2692c4fbe0ee4e69eddaabb3398553
devuri <devuriko@gmail.com>
2020-08-16 02:32:00 -0500

    Fix 404, add sidebar

--- 404.php
+++ 404.php
@@ -8,38 +8,41 @@
  */
 
 get_header(); ?>
-	<main id="primary" class="site-main container">
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'brisko' ); ?></h1>
-			</header><!-- .page-header -->
-			<div class="page-content">
+<main id="primary" class="site-main container bg-white">
+	<div class="row">
+		<div class="col-md-8 primary-content">
+			<section class="error-404 not-found">
+				<header class="page-header">
+					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'brisko' ); ?></h1>
+				</header><!-- .page-header -->
 				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'brisko' ); ?></p>
 					<?php get_search_form();
 					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'brisko' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-					<?php
-					/* translators: %1$s: smiley */
-					$brisko_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'brisko' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$brisko_archive_content" );
-					the_widget( 'WP_Widget_Tag_Cloud' ); ?>
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
-	</main><!-- #main -->
+					?><div class="widget widget_categories">
+							<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'brisko' ); ?></h2>
+								<ul><?php
+									wp_list_categories(
+										array(
+											'orderby'    => 'count',
+											'order'      => 'DESC',
+											'show_count' => 1,
+											'title_li'   => '',
+											'number'     => 10,
+										)
+									);
+							?></ul>
+				</div><!-- .widget -->
+				<?php
+				/* translators: %1$s: smiley */
+				$brisko_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'brisko' ), convert_smilies( ':)' ) ) . '</p>';
+				the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$brisko_archive_content" );
+				the_widget( 'WP_Widget_Tag_Cloud' ); ?>
+		</div><!-- col 8 -->
+	<div class="col-md-4">
+		<div class="sidebar mb-4  avs-sidebar pad-left-1m">
+			<?php get_sidebar(); ?>
+			</div><!-- sidebar -->
+		</div><!-- col 4 -->
+	</div><!-- row -->
+</main><!-- #main -->
 <?php get_footer();
