b4ef85b744f62500446f9000f3714d8ac23e0a99
devuri <devuriko@gmail.com>
2020-10-26 00:12:25 -0500

    new brisko_theme_mod()

--- footer.php
+++ footer.php
@@ -18,8 +18,8 @@
 			 * Brisko Theme credit
 			 * @var [type]
 			 */
-			if ( get_option('brisko_options' , false ) !== false ) {
-				$copyright = '<span class="brisko-footer-copyright">'.get_option('brisko_options')['copyright'].'</span>';
+			if ( brisko_theme_mod( 'copyright' ) !== false ) {
+				$copyright = '<span class="brisko-footer-copyright">' . brisko_theme_mod( 'copyright' ) . '</span>';
 			} else {
 				$copyright = get_bloginfo('name');
 			}
