01fed87f95e1280cabfb95e8603cf0ae32be38a1
devuri <devuriko@gmail.com>
2020-11-02 11:55:52 -0500

    Add support for custom website width

--- src/Customize/Customizer.php
+++ src/Customize/Customizer.php
@@ -70,6 +70,58 @@ class Customizer
 
 		$this->brisko_options( $wp_customize );
 
+		// Separator General Settings.
+		( new Control() )->separator( $wp_customize, esc_html__( 'General', 'brisko' ) );
+
+		/**
+		 * Link Color
+		 */
+		$wp_customize->add_setting(
+			'link_color', array(
+				'capability'        => 'manage_options',
+				'default'           => '#E4584B',
+				'transport'         => self::$transport,
+				'sanitize_callback' => 'sanitize_hex_color',
+			)
+		);
+
+		$wp_customize->add_control(
+			new \WP_Customize_Color_Control(
+				$wp_customize, 'link_color',
+				array(
+					'label'       => esc_html__( 'Link Color', 'brisko' ),
+					'description' => esc_html__( 'Select a color', 'brisko' ),
+					'section'     => self::$section,
+				)
+			)
+		);
+
+		/**
+		 * Header Image
+		 */
+		$wp_customize->add_setting(
+			'website_width', array(
+				'sanitize_callback' => 'absint',
+				'default'           => 'entire-site',
+			)
+		);
+
+		$wp_customize->add_control(
+			'website_width', array(
+				'label'       => esc_html__( 'Site Width', 'brisko' ),
+				'description' => esc_html__( 'website with, only applies with Box layout', 'brisko' ),
+				'section'     => self::$section,
+				'type'        => 'select',
+				'choices'     => array(
+					1200 => esc_html__( 'XXL (1200)', 'brisko' ),
+					1140 => esc_html__( 'XL (1140)', 'brisko' ),
+					960  => esc_html__( 'L (960)', 'brisko' ),
+					720  => esc_html__( 'M (720)', 'brisko' ),
+					540  => esc_html__( 'S (540)', 'brisko' ),
+				),
+			)
+		);
+
 		// Separator Header Image Settings.
 		( new Control() )->separator( $wp_customize, esc_html__( 'Header Image', 'brisko' ) );
 		/**
@@ -119,31 +171,6 @@ class Customizer
 			)
 		);
 
-		// Separator General Settings.
-		( new Control() )->separator( $wp_customize, esc_html__( 'General', 'brisko' ) );
-
-		/**
-		 * Link Color
-		 */
-		$wp_customize->add_setting(
-			'link_color', array(
-				'capability'        => 'manage_options',
-				'default'           => '#E4584B',
-				'transport'         => self::$transport,
-				'sanitize_callback' => 'sanitize_hex_color',
-			)
-		);
-
-		$wp_customize->add_control(
-			new \WP_Customize_Color_Control(
-				$wp_customize, 'link_color',
-				array(
-					'label'       => esc_html__( 'Link Color', 'brisko' ),
-					'description' => esc_html__( 'Select a color', 'brisko' ),
-					'section'     => self::$section,
-				)
-			)
-		);
 		// Separator Pages Settings.
 		( new Control() )->separator( $wp_customize, esc_html__( 'Pages', 'brisko' ) );
 
