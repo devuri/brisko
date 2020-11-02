54017032db528fb5b4f16e693a4ded90f813ecbb
devuri <devuriko@gmail.com>
2020-11-02 03:46:25 -0500

    move setup functions to Activate

---
+++ src/Activate.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace Brisko;
+
+final class Activate
+{
+	/**
+	 * Private $instance
+	 *
+	 * @var $instance
+	 */
+	private static $instance;
+
+	/**
+	 * Singleton
+	 *
+	 * @return object
+	 */
+	public static function init() {
+
+		if ( ! isset( self::$instance ) ) {
+			self::$instance = new Activate();
+		}
+		return self::$instance;
+	}
+
+	/**
+	 * Theme Setup
+	 */
+	private function __construct() {
+		add_action( 'after_setup_theme', array( $this, 'brisko_setup' ) );
+		add_action( 'after_setup_theme', array( $this, 'brisko_content_width' ), 0 );
+		add_action( 'widgets_init', array( $this, 'brisko_widgets_init' ) );
+	}
+
+		/**
+		 * Sets up theme defaults and registers support for various WordPress features.
+		 *
+		 * Note that this function is hooked into the after_setup_theme hook, which
+		 * runs before the init hook. The init hook is too late for some features, such
+		 * as indicating support for post thumbnails.
+		 */
+		public function brisko_setup() {
+			/*
+			 * Make theme available for translation.
+			 */
+			load_theme_textdomain( 'brisko' );
+
+			// Add default posts and comments RSS feed links to head.
+			add_theme_support( 'automatic-feed-links' );
+
+			/*
+			 * Let WordPress manage the document title.
+			 * By adding theme support, we declare that this theme does not use a
+			 * hard-coded <title> tag in the document head, and expect WordPress to
+			 * provide it for us.
+			 */
+			add_theme_support( 'title-tag' );
+
+			/*
+			 * Enable support for Post Thumbnails on posts and pages.
+			 *
+			 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+			 */
+			add_theme_support( 'post-thumbnails' );
+
+			// This theme uses wp_nav_menu() in one location.
+			register_nav_menus(
+				array(
+					'menu-1' => esc_html__( 'Primary', 'brisko' ),
+				)
+			);
+
+			/*
+			 * Switch default core markup for search form, comment form, and comments
+			 * to output valid HTML5.
+			 */
+			add_theme_support(
+				'html5',
+				array(
+					'search-form',
+					'comment-form',
+					'comment-list',
+					'gallery',
+					'caption',
+					'style',
+					'script',
+				)
+			);
+
+			// Set up the WordPress core custom background feature.
+			add_theme_support(
+				'custom-background',
+				apply_filters(
+					'brisko_custom_background_args',
+					array(
+						'default-color' => 'ffffff',
+						'default-image' => '',
+					)
+				)
+			);
+
+			// Add theme support for selective refresh for widgets.
+			add_theme_support( 'customize-selective-refresh-widgets' );
+
+			/**
+			 * Add support for core custom logo.
+			 *
+			 * @link https://codex.wordpress.org/Theme_Logo
+			 */
+			add_theme_support(
+				'custom-logo',
+				array(
+					'height'      => 250,
+					'width'       => 250,
+					'flex-width'  => true,
+					'flex-height' => true,
+				)
+			);
+		}
+
+
+	/**
+	 * Set the content width in pixels, based on the theme's design and stylesheet.
+	 *
+	 * Priority 0 to make it available to lower priority callbacks.
+	 *
+	 * @global int $content_width
+	 */
+	public function brisko_content_width() {
+		// This variable is intended to be overruled from themes.
+		// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
+		// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
+		$GLOBALS['content_width'] = apply_filters( 'brisko_content_width', 640 );
+	}
+
+	/**
+	 * Register widget area.
+	 *
+	 * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+	 */
+	public function brisko_widgets_init() {
+		register_sidebar(
+			array(
+				'name'          => esc_html__( 'Sidebar', 'brisko' ),
+				'id'            => 'sidebar-1',
+				'description'   => esc_html__( 'Add widgets here.', 'brisko' ),
+				'before_widget' => '<section id="%1$s" class="widget %2$s">',
+				'after_widget'  => '</section>',
+				'before_title'  => '<h2 class="widget-title">',
+				'after_title'   => '</h2>',
+			)
+		);
+	}
+
+}
