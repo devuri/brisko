1aba4c2254bd1c3891560aa7c7403409ce1cd835
devuri <devuriko@gmail.com>
2020-11-04 10:46:38 -0500

    fix: Fix phpcs

--- src/View/Page.php
+++ src/View/Page.php
@@ -18,12 +18,12 @@ class Page extends Layout
 		 * Page content
 		 */
 		while ( have_posts() ) :
- 			the_post();
- 			get_template_part( 'template-parts/content', 'page' );
- 			if ( comments_open() || get_comments_number() ) :
- 				comments_template();
- 			endif;
- 		endwhile;
+			the_post();
+			get_template_part( 'template-parts/content', 'page' );
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+		endwhile;
 
 		self::footer();
 	}
