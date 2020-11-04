7e8d91e6ba12564c049950ced192fde012ef779d
devuri <devuriko@gmail.com>
2020-11-04 15:01:48 -0500

    fix: Fix phpcs

--- src/View/FullWidthPage.php
+++ src/View/FullWidthPage.php
@@ -16,12 +16,12 @@ class FullWidthPage extends Layout
 		 * Page content
 		 */
 		while ( have_posts() ) :
- 			the_post();
- 			get_template_part( 'template-parts/content', 'full-width' );
- 			if ( comments_open() || get_comments_number() ) :
- 				comments_template();
- 			endif;
- 		endwhile;
+			the_post();
+			get_template_part( 'template-parts/content', 'full-width' );
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+		endwhile;
 
 		self::footer();
 	}
