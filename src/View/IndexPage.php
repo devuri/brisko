29d290d3c3e63060c9d95ad2a88b6213ce11a071
devuri <devuriko@gmail.com>
2020-11-04 13:00:49 -0500

    fix: Fix phpcs

--- src/View/IndexPage.php
+++ src/View/IndexPage.php
@@ -16,23 +16,23 @@ class IndexPage extends Layout
 		 * Index Page content
 		 */
 		if ( have_posts() ) :
- 			if ( is_home() && ! is_front_page() ) : ?>
- 				<header>
- 					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
- 				</header>
- 				<?php
+			if ( is_home() && ! is_front_page() ) : ?>
+				<header>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
+				<?php
 			endif;
- 			/* Start the Loop */
- 			while ( have_posts() ) :
- 				the_post();
- 				get_template_part( 'template-parts/content', get_post_type() );
- 			endwhile;
- 				the_posts_navigation();
- 			else :
- 				get_template_part( 'template-parts/content', 'none' );
- 		endif;
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
+				get_template_part( 'template-parts/content', get_post_type() );
+			endwhile;
+				the_posts_navigation();
+			else :
+				get_template_part( 'template-parts/content', 'none' );
+		endif;
 
-		self::footer();
+			self::footer();
 	}
 
 }
