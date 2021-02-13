8c4ff80f147181385399a79c3aba102d60f16949
devuri <devuriko@gmail.com>
2021-02-12 20:29:02 -0500

    feat: add new Advanced Settings

--- src/Customize/Controls/Control.php
+++ src/Customize/Controls/Control.php
@@ -39,8 +39,9 @@ class Control {
 	 * @param WP_Customize_Manager $wp_customize Theme Customizer object.
 	 * @param string               $label .
 	 * @param string               $section .
+	 * @param string               $description .
 	 */
-	public function header_title( $wp_customize, $label = 'Custom Title', $section = null ) {
+	public function header_title( $wp_customize, $label = 'Custom Title', $section = null, $description = null ) {
 
 		$id = sanitize_title( $label );
 		$id = str_replace( '-', '_', $id );
@@ -55,8 +56,9 @@ class Control {
 			new HeaderControl(
 				$wp_customize, $id,
 				array(
-					'label'   => esc_html( $label ),
-					'section' => $section,
+					'label'       => esc_html( $label ),
+					'section'     => $section,
+					'description' => $description,
 				)
 			)
 		);
--- src/Customize/Sections.php
+++ src/Customize/Sections.php
@@ -31,6 +31,7 @@ final class Sections {
 		$this->sections['blog']       = 'Blog';
 		$this->sections['posts']      = 'Posts';
 		$this->sections['footer']     = 'Footer';
+		$this->sections['advanced']   = 'Advanced';
 
 		if ( is_child_theme() ) :
 			$this->sections['child'] = 'Child Theme';
