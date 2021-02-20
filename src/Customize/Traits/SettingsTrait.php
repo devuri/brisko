f333f6c1004fcc69739ee95850e82a45556a197d
devuri <devuriko@gmail.com>
2021-02-20 11:46:59 -0500

    add install_plugin to the settings trait

--- src/Customize/Traits/SettingsTrait.php
+++ src/Customize/Traits/SettingsTrait.php
@@ -26,4 +26,15 @@ trait SettingsTrait {
 		return 'brisko_section_' . self::short_name();
 	}
 
+	/**
+	 * Brisko Section
+	 */
+	public static function install_plugin() {
+
+		$button  = '<a href="' . esc_url( network_admin_url( 'plugin-install.php?tab=search&type=tag&s=brisko' ) ) . '" class="button button-secondary">';
+		$button .= 'Install Plugin';
+		$button .= '</a>';
+		return "<p>Get Advanced Options and Custom features. Install the Brisko Elements Plugin.</p>$button";
+	}
+
 }
