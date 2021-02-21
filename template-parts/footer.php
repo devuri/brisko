d3e54adb59b5b63ada20aae7e0a68dc83890ef53
devuri <devuriko@gmail.com>
2021-02-20 23:52:23 -0500

    feat: new footer_width() option

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -1,7 +1,7 @@
 <?php brisko()->action( 'brisko_before_footer' ); ?>
 <footer id="colophon" class="site-footer this-site-footer <?php Brisko\Theme::options()->footer_top_margin(); ?>">
 	<?php brisko_footer(); ?>
-	<div align="center" class="site-info container">
+	<div class="site-info <?php Brisko\Theme::options()->footer_width(); ?>">
 		<div class="brisko-theme-credit">
 			<?php Brisko\Theme::footer_credit(); ?>
 			<?php do_action( 'brisko_footer_credit' ); ?>
