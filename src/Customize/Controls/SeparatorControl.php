bad251b0aecd488aa87e90bffe5213ba8b1d091f
devuri <devuriko@gmail.com>
2021-02-22 01:53:58 -0500

    feat: add description to the separator control

--- src/Customize/Controls/SeparatorControl.php
+++ src/Customize/Controls/SeparatorControl.php
@@ -17,10 +17,14 @@ class SeparatorControl extends \WP_Customize_Control {
 	public function render_content() {
 		?><br/>
 		<div class="customize-section-title">
-			<h3 style="color: #1c66c2;font-weight: 400;border-top: 1px solid #ddd;padding: 5px 5px 5px 14px;font-size: initial;">
+			<h3 style="color: #1c66c2;font-weight: 400;border-top: 1px solid #ddd;padding: 5px 5px 0px 14px;font-size: initial;">
 				<?php echo esc_html( $this->label ); ?>
 			</h3>
+			<span class="description customize-control-description" style="padding-left:14px;">
+				<?php echo wp_kses_post( $this->description ); ?>
+			</span>
 		</div>
+
 		<?php
 	}
 }
