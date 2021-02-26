eab5818472de76c47a0202b003ecb55510254bc2
devuri <devuriko@gmail.com>
2021-02-26 13:47:47 -0500

    fix: Fix the Button fade out effect.

--- js/customizer/toggle-button-control.js
+++ js/customizer/toggle-button-control.js
@@ -25,7 +25,7 @@
 
 		$.each( toggleControlIds, function( index, control_name ) {
 			customize( control_name, function( value ) {
-				var controlTitle = customize.control( control_name ).container.find( '.customize-control-title' ); // Get control  title.
+				var controlTitle = customize.control( control_name ).container.find( '.toggle-control-title' ); // Get control  title.
 				// 1. On loading.
 				controlTitle.toggleClass('disabled-control-title', !value.get() );
 				// 2. Binding to value change.
