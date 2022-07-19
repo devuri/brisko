ad1eeb477f5a2450a7d58b7c7bc3cc509bbaa426
Uriel Wilson <devuri@users.noreply.github.com>
2022-07-19 08:45:49 -0500

    Update Content.php

--- src/View/Content.php
+++ src/View/Content.php
@@ -8,30 +8,28 @@ class Content
 {
     use Instance;
 
-	/**
-	 * Display content only.
-	 *
-	 * pure content will bypass `the_content` and use `get_the_content`
-	 * works better for pure content management and will not break html.
-	 *
-	 * note: filters on `the_content` will not work.
-	 *
-	 */
+    /**
+     * Display content only.
+     *
+     * pure content will bypass `the_content` and use `get_the_content`
+     * works better for pure content management and will not break html.
+     *
+     * note: filters on `the_content` will not work.
+     */
     public function view()
     {
-		if ( get_theme_mod( 'enable_pure_content', false ) ) {
+        if ( get_theme_mod( 'enable_pure_content', false ) ) {
             echo wp_kses_post( $this->the_content() );
         } else {
             the_content();
         }
     }
 
-	/**
+    /**
      * Display content.
      */
     protected function the_content()
     {
-		return get_the_content( null, false );
+        return get_the_content( null, false );
     }
-
 }
