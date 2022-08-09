0fbba8d01e856ec56919d60103e40ab7d01caa8f
devuri <devuriko@gmail.com>
2022-08-08 23:42:55 -0500

    fix uikit and add uikit icons

--- src/Setup/Scripts.php
+++ src/Setup/Scripts.php
@@ -30,6 +30,14 @@ class Scripts implements EnqueueInterface
             wp_enqueue_script( 'brisko-bootstrap' );
         }
 
+		if ( true === get_theme_mod( 'enable_uikit_js', false ) ) {
+            wp_enqueue_script( 'uikit-js' );
+        }
+
+		if ( true === get_theme_mod( 'enable_uikit_icons_js', false ) ) {
+            wp_enqueue_script( 'uikit-icons' );
+        }
+
         if ( true === get_theme_mod( 'enable_navigation_js', true ) ) {
             wp_enqueue_script( 'brisko-navigation' );
         }
@@ -44,59 +52,26 @@ class Scripts implements EnqueueInterface
     }
 
     /**
-     * Setup static JS files.
-     *
-     * @return string[] .
-     *
-     * @psalm-return array{'brisko-popper': string, 'brisko-bootstrap': string, 'brisko-uikit': string, 'brisko-navigation': string, 'brisko-smooth-scroll': string}
-     */
-    public static function js_files()
-    {
-        return [
-
-            // bootstrap 5.
-            'brisko-bootstrap5'        => Assets::uri( 'bootstrap5/js/bootstrap.min.js' ),
-            'brisko-bootstrap5-bundle' => Assets::uri( 'bootstrap5/js/bootstrap.bundle.min.js' ),
-            'brisko-bootstrap5-esm'    => Assets::uri( 'bootstrap5/js/bootstrap.esm.min.js' ),
-
-            // bootstrap 4.
-            'brisko-popper'            => Assets::uri( 'js/bootstrap/popper.min.js' ),
-            'brisko-bootstrap'         => Assets::uri( 'js/bootstrap/bootstrap.min.js' ),
-            'brisko-uikit'             => Assets::uri( 'js/uikit.min.js' ),
-            'brisko-navigation'        => Assets::uri( 'js/navigation.min.js' ),
-            'brisko-smooth-scroll'     => Assets::uri( 'js/smooth-scroll.js' ),
-        ];
-    }
-
-    /**
      * Enqueue styles and script.
      *
      * @return void
      */
     public function register()
     {
-        wp_register_script( 'brisko-popper', Assets::uri( 'bootstrap/js/popper.min.js' ), [ 'jquery' ], self::ver( 'brisko-popper' ), true );
-        wp_register_script( 'brisko-bootstrap', Assets::uri( 'bootstrap/js/bootstrap.min.js' ), [ 'jquery' ], self::ver( 'brisko-bootstrap' ), true );
+        wp_register_script( 'brisko-popper', Assets::uri( 'bootstrap/js/popper.min.js' ), [ 'jquery' ], md5( 'brisko-popper' ), true );
+        wp_register_script( 'brisko-bootstrap', Assets::uri( 'bootstrap/js/bootstrap.min.js' ), [ 'jquery' ], md5( 'brisko-bootstrap' ), true );
+
+		// uikit.
+		wp_register_script( 'uikit-js', Assets::uri( 'uikit/js/uikit.min.js' ), [], md5( 'uikit-js' ), true );
+		wp_register_script( 'uikit-icons', Assets::uri( 'uikit/js/uikit-icons.min.js' ), [], md5( 'uikit-icons' ), true );
 
         // bootstrap 5
-        wp_register_script( 'brisko-bootstrap5', Assets::uri( 'bootstrap5/js/bootstrap.min.js' ), [], self::ver( 'brisko-bootstrap5' ), true );
-        wp_register_script( 'brisko-bootstrap5-bundle', Assets::uri( 'bootstrap5/js/bootstrap.bundle.min.js' ), [], self::ver( 'brisko-bootstrap5-bundle' ), true );
-        wp_register_script( 'brisko-bootstrap5-esm', Assets::uri( 'bootstrap5/js/bootstrap.esm.min.js' ), [], self::ver( 'brisko-bootstrap5-esm' ), true );
+        wp_register_script( 'brisko-bootstrap5', Assets::uri( 'bootstrap5/js/bootstrap.min.js' ), [], md5( 'brisko-bootstrap5' ), true );
+        wp_register_script( 'brisko-bootstrap5-bundle', Assets::uri( 'bootstrap5/js/bootstrap.bundle.min.js' ), [], md5( 'brisko-bootstrap5-bundle' ), true );
+        wp_register_script( 'brisko-bootstrap5-esm', Assets::uri( 'bootstrap5/js/bootstrap.esm.min.js' ), [], md5( 'brisko-bootstrap5-esm' ), true );
 
-        wp_register_script( 'brisko-navigation', Assets::uri( 'js/navigation.js' ), [], self::ver( 'brisko-navigation' ), true );
-        wp_register_script( 'brisko-smooth-scroll', Assets::uri( 'js/smooth-scroll.js' ), [], self::ver( 'brisko-smooth-scroll' ), true );
-    }
-
-    /**
-     * Set file version.
-     *
-     * @param string $handle .
-     *
-     * @return string .
-     */
-    public static function ver( $handle )
-    {
-        return md5( self::js_files()[ $handle ] );
+        wp_register_script( 'brisko-navigation', Assets::uri( 'js/navigation.min.js' ), [], md5( 'brisko-navigation' ), true );
+        wp_register_script( 'brisko-smooth-scroll', Assets::uri( 'js/smooth-scroll.js' ), [], md5( 'brisko-smooth-scroll' ), true );
     }
 
     protected static function bootstrap5()
