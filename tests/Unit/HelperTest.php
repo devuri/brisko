dc32e0bbdfaba2cb3a5173591fd347b8ebe70e92
devuri <devuriko@gmail.com>
2022-08-19 23:06:51 -0500

    test: Adds test for brisko() object

---
+++ tests/Unit/HelperTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\BriskoTest;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class HelperTest extends BriskoTest
+{
+    public function test_brisko_object_attributes(): void
+    {
+		$brisko = brisko();
+
+		static::assertIsObject($brisko);
+
+		static::assertObjectHasAttribute('activate',$brisko);
+		static::assertObjectHasAttribute('assets',$brisko);
+		static::assertObjectHasAttribute('body',$brisko);
+		static::assertObjectHasAttribute('head',$brisko);
+		static::assertObjectHasAttribute('jetpack',$brisko);
+		static::assertObjectHasAttribute('customizer',$brisko);
+    }
+}
