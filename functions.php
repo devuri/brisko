1119a9878fa0366721360420c5e65c088173ea3e
devuri <devuri@users.noreply.github.com>
2023-05-25 12:36:44 -0500

    build 3.7.2

--- functions.php
+++ functions.php
@@ -15,28 +15,3 @@ require __DIR__ . '/vendor/autoload.php';
 
 // Theme setup.
 brisko()->setup();
-
-// load customizer preview.
-add_action( 'customize_preview_init', 'brisko_customize_preview_js' );
-
-/*
- * Compatibility for Elementor Header and Footer.
- *
- * @link https://developers.elementor.com/theme-locations-api/registering-locations
- */
-if ( did_action( 'elementor/loaded' ) ) {
-    add_action(
-        'elementor/theme/register_locations',
-        function ( $elementor_theme_manager ) {
-            $elementor_theme_manager->register_location( 'header' );
-            $elementor_theme_manager->register_location( 'footer' );
-        } );
-}
-
-/*
- * Disable wpautop.
- *
- * @link https://developer.wordpress.org/reference/functions/wpautop/
- * @link https://stackoverflow.com/questions/20760598/how-to-remove-extra-p-p-tags-in-wordpress-post-and-pages
- */
-remove_filter( 'the_content', 'wpautop' );
