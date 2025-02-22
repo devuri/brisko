feb13e4eec9208faf9c53607bbcaa48ae39ce3ba
devuri <devuri@users.noreply.github.com>
2025-02-21 21:53:33 -0500

    fix: remove edit link

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -31,24 +31,7 @@ wp_link_pages(
 	</div><!-- .entry-content -->
 	<?php if ( get_edit_post_link() ) { ?>
 		<footer class="entry-footer">
-			<?php
-    edit_post_link(
-        \sprintf(
-            wp_kses(
-                // translators: %s: Name of current post. Only visible to screen readers
-                __( 'Edit <span class="screen-reader-text">%s</span>', 'brisko' ),
-                [
-                    'span' => [
-                        'class' => [],
-                    ],
-                ]
-            ),
-            wp_kses_post( get_the_title() )
-        ),
-        '<span class="edit-link">',
-        '</span>'
-    );
-	    ?>
+			<!-- edit-link -->
 		</footer><!-- .entry-footer -->
 	<?php } ?>
 </article><!-- #post-<?php the_ID(); ?> -->
